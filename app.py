6f448ae06b7db784e428f7c3a3c404ec6fcfe795
yaomingyong <icheeringsoul@163.com>
2020-04-20 23:55:25 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 
 @app.route('/')
