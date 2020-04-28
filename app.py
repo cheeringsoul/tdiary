2ee43cc19bed63678fe9932d0bcc0b11f802b639
yaomingyong <icheeringsoul@163.com>
2020-04-28 13:12:19 +0800

    update

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-from flask import Flask
-from flask_login import LoginManager
-
+from flask import Flask, session, redirect, url_for, request
+from markupsafe import escape
 
 app = Flask(__name__)
-login_manager = LoginManager(app)
 
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+app.run()
---
+++ view/diary.py
@@ -0,0 +1,9 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+
+bp = Blueprint('diary', __name__, url_prefix='/diary')
+
+
